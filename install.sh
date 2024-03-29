8b233e17f95ae6998e0bb8ece9cc012ebf1de53a
Stefan Bauer <stefan.82549@web.de>
2024-03-29 14:50:48 +0100

    all configs

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+# Full update and upgrade of Kali Linux
+sudo apt update && sudo apt full-upgrade -y
+
+
+# Install tools and i3 window manager
+sudo apt-get install -y gobuster
+sudo apt-get install -y Seclists
+sudo apt-get install -y xorg lightdm lightdm-gtk-greeter i3 i3-wm i3lock i3status i3blocks dmenu
+sudo apt-get install -y flameshot arc-theme feh python3-pip rofi papyrus-icon-theme
+
+# Load Fonts
+mkdir -p ~/.local/share/fonts/
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
+
+unzip Iosevka.zip -d ~/.local/share/fonts/
+unzip RobotoMono.zip -d ~/.local/share/fonts/
+
+
+# Update font cache
+fc -cache -fv
+
+
+# Install alacritty via package manager
+wget https://github.com/barnumbirr/alacritty-debian/releases/download/v0.10.0-rc4-1/alacritty_0.10.0-rc4-1_amd64_bullseye.deb
+sudo dpkg -i alacritty_0.10.0-rc4-1_amd64_bullseye.deb
+sudo apt install -f
+
+
+# Install pywal
+pip3 install pywal
+
+
+# Create directorys for the configs
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/rofi
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.wallpaper
+
+cp .config/i3/config ~/.config/i3/config
+cp .config/rofi/config ~/.config/rofi/config
+cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp -r .wallpaper ~/.wallpaper
