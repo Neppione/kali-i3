bf05424c04155fa3964577de1b6654d526650ca7
Neppione <stefan.82549@web.de>
2024-07-08 17:33:03 +0200

    updated files

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 
+
 #Update && Upgrade && Install
-
-
-#sudo apt update && sudo apt full-upgrade -y
+sudo apt update && sudo apt full-upgrade -y
 
 sudo apt-get install -y gobuster
 sudo apt-get install -y seclists
@@ -18,12 +17,13 @@ sudo apt remove -y i3lock
 sudo apt-get install -y i3lock-color
 
 
+
 #Unzip Rockyou
 sudo gunzip /usr/share/wordlists/rockyou.txt.gz rockyou.txt
 
 
-#Fonts
 
+#Fonts
 mkdir -p ~/.local/share/fonts/
 
 
@@ -34,16 +34,13 @@ unzip Iosevka.zip -d ~/.local/share/fonts/
 unzip RobotoMono.zip -d ~/.local/share/fonts/
 
 
+
 #Update font cache, force, verbose
 fc-cache -fv
 
 
 
-
-
-
 #Alacritty
-
 wget https://github.com/barnumbirr/alacritty-debian/releases/download/v0.12.0-1/alacritty_0.12.0_amd64_bullseye.deb
 sudo dpkg -i alacritty_0.12.0_amd64_bullseye.deb
 sudo apt install -f
@@ -51,20 +48,12 @@ sudo apt install -f
 
 
 #Rofi Theme
-
 wget https://github.com/newmanls/rofi-themes-collection/blob/master/themes/squared-nord.rasi
 mv squared-nord.rasi /usr/share/rofi/themes
 
 
 
-
-
-
-
-
-
 #Directories
-
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/rofi
 mkdir -p ~/.config/alacritty
@@ -72,7 +61,6 @@ mkdir -p ~/.config/alacritty
 
 
 #Copy Files
-
 cp .config/i3/config ~/.config/i3/config
 cp .config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
 cp .config/rofi/rofi.conf ~/.config/rofi/rofi.conf
