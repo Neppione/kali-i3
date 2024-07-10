648a80ca499a885234e7ac0b24cec4f4628229da
Neppione <stefan.82549@web.de>
2024-07-10 19:30:12 +0200

    updated files

--- install.sh
+++ install.sh
@@ -72,6 +72,8 @@ unzip RobotoMono.zip -d ~/.local/share/fonts/
 #Update font cache, force, verbose
 find ~/.local/share/fonts -name "README.md" -type f -delete
 fc-cache -f -v
+find ~/.local/share/fonts -name "README.md" -type f -delete
+fc-cache -f -v
 
 
 
